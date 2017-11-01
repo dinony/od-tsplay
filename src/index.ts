38891cd03ea85bf99e793c1106646f115b6d5043
Onur D <contact@dinony.com>
2017-11-01 11:48:34 +0100

    Add angular config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-const elem = document.querySelector('.test') as Element
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule, Component} from '@angular/core'
 
-elem.innerHTML = ':)'
+@Component({
+  'selector': 'od-app',
+  'template': ':)'
+})
+export class AppShell {}
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppShell],
+  bootstrap:[AppShell]
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
--- systemjs.config.js
+++ systemjs.config.js
@@ -1,4 +1,16 @@
 SystemJS.config({
   paths: {'npm:' : 'node_modules/'},
+  map: {
+    '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+    '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+    '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+    '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+
+    'rxjs': 'npm:rxjs'
+  },
+  packages: {
+    rxjs: {defaultExtension: 'js'}
+  },
   warnings: true
 })
